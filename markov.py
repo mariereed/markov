2fdc7eca6c1a0c9a788ac7c4465c37c93c5c88b3
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-10 16:02:42 -0700

    Working on changing n, currently broken.

--- markov.py
+++ markov.py
@@ -22,7 +22,7 @@ def open_and_read_file(file_path):
     return text
 
 
-def make_chains(file_as_string):
+def make_chains(file_as_string, n):
     """Take input text as string; return dictionary of Markov chains.
 
     A chain will be a key that consists of a tuple of (word1, word2)
@@ -51,47 +51,65 @@ def make_chains(file_as_string):
 
     words = file_as_string.split()
     words.append(None)
-
-    for i in range(len(words) - 1):
-        chains[words[i], words[i + 1]] = []
-
-    for word1, word2 in chains:
-        for i in range(len(words) - 2):
-            if words[i] == word1 and words[i + 1] == word2:
-                chains[(word1, word2)].append(words[i + 2])
-
+    
     #otherway
+
+    # for i in range(len(words) - 1):
+    #     chains[words[i], words[i + 1]] = []
+
+    # for word1, word2 in chains:
+    #     for i in range(len(words) - 2):
+    #         if words[i] == word1 and words[i + 1] == word2:
+    #             chains[(word1, word2)].append(words[i + 2])
+
+    # return chains
+
     # for i in range(len(words)-2):
     #     key = (words[i], words[i+1])
     #     value = words[i+2]
     #     chains[key] = chains.get(key, [])
     #     chains[key].append(value)
 
-    return chains
+    # return chains
 
+    for i in range(len(words)-n):
+        key = []
+        for j in range(n):
+            key.append(words[i + j])
+        key = tuple(key)
+        value = words[i+n]
+        chains[key] = chains.get(key, [])
+        chains[key].append(value)
+
+    return chains
 
 def make_text(chain):
     """Return text from chains."""
 
-    key = choice(chain.keys())
+    start_key = choice(chain.keys())
+    print start_key
     # start_key1, start_key2 = key
     # words = [start_key1, start_key2]
-    words = list(key)
+    words = list(start_key)
 
     while True:
-        next_word = choice(chain[key])
+        next_word = choice(chain[start_key])
         if not next_word:
             break
-        first_word, second_word = key
-        key = (second_word, next_word)
+        start_key2 = list(start_key)
+        start_key2.pop(0)
+        start_key2.append(next_word)
+        print start_key2
         words.append(next_word)
+        print words
+
 
     return " ".join(words)
 
 
 filename = sys.argv[1]
 file_as_string = open_and_read_file(filename)
-my_chain = make_chains(file_as_string)
+my_chain = make_chains(file_as_string, 5)
 print make_text(my_chain)
 
 
